6d6c58e7caaa2f9d31eeb2dc507a3ff5fde94607
kaio0liveira <kaiooliveira466@gmail.com>
2021-07-27 11:40:41 -0300

    Consumidores

---
+++ src/br/com/paradigmaFuncionalJava/Comsumidores.java
@@ -0,0 +1,14 @@
+package br.com.paradigmaFuncionalJava;
+
+import java.util.function.Consumer;
+
+public class Comsumidores {
+	public static void main(String[] args) {
+		
+		//Consumer<String> imprimirUmaFrase = System.out::println;
+		
+		Consumer<String> imprimirUmaFrase = frase -> System.out.println(frase);
+		
+		imprimirUmaFrase.accept("Hello world");
+	}
+}
